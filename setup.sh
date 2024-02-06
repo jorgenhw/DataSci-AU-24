e099a7621a1ebbc96d63434547b7cc4559b9ca48
jorgenhw <jrgenhwn1@gmail.com>
2024-02-06 13:55:47 +0100

    first commit

---
+++ setup.sh
@@ -0,0 +1,8 @@
+python3 -m venv venv_data_science_2024
+
+#activate
+source ./venv_data_science_2024/bin/activate
+
+# Install requirements
+python3 -m pip install --upgrade pip
+pip3 install -r requirements.txt
