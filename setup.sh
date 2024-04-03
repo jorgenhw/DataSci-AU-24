43eef02e82385cbb1d66d993b9317da41e1c4eae
jorgenhw <jrgenhwn1@gmail.com>
2024-04-03 09:10:21 +0200

    1

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 python3 -m venv venv_data_science_2024
 
 #activate
-source ./venv_data_science_2024/bin/activate
+source venv_data_science_2024/bin/activate
 
 # Install requirements
 python3 -m pip install --upgrade pip
